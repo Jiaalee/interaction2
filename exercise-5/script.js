3f6978b223d219e4abc9c946801aff76232600c7
leejia0704 <145470836+Jiaalee@users.noreply.github.com>
2024-03-29 02:56:46 -0400

    exercise 5

---
+++ exercise-5/script.js
@@ -0,0 +1,127 @@
+let tri1 = document.getElementById("tri1");
+let tri2 = document.getElementById("tri2");
+let tri3 = document.getElementById("tri3");
+let tri4 = document.getElementById("tri4");
+let tri5 = document.getElementById("tri5");
+
+let tri6 = document.getElementById("tri6");
+let tri7 = document.getElementById("tri7");
+let tri8 = document.getElementById("tri8");
+let tri9 = document.getElementById("tri9");
+let tri10 = document.getElementById("tri10");
+
+let tri11 = document.getElementById("tri11");
+let tri12 = document.getElementById("tri12");
+let tri13 = document.getElementById("tri13");
+let tri14 = document.getElementById("tri14");
+let tri15 = document.getElementById("tri15");
+
+let tri16 = document.getElementById("tri16");
+let tri17 = document.getElementById("tri17");
+let tri18 = document.getElementById("tri18");
+let tri19 = document.getElementById("tri19");
+let tri20 = document.getElementById("tri20");
+
+let colors = ["skyblue", "babypink", "white", "yellowgreen", "coral"] 
+
+let randomColor1 = colors[Math.floor(Math.random() * colors.length)];
+let randomColor2 = colors[Math.floor(Math.random() * colors.length)];
+let randomColor3 = colors[Math.floor(Math.random() * colors.length)];
+let randomColor4 = colors[Math.floor(Math.random() * colors.length)];
+let randomColor5 = colors[Math.floor(Math.random() * colors.length)];
+
+let randomColor6 = colors[Math.floor(Math.random() * colors.length)];
+let randomColor7 = colors[Math.floor(Math.random() * colors.length)];
+let randomColor8 = colors[Math.floor(Math.random() * colors.length)];
+let randomColor9 = colors[Math.floor(Math.random() * colors.length)];
+let randomColor10 = colors[Math.floor(Math.random() * colors.length)];
+
+let randomColor11 = colors[Math.floor(Math.random() * colors.length)];
+let randomColor12 = colors[Math.floor(Math.random() * colors.length)];
+let randomColor13 = colors[Math.floor(Math.random() * colors.length)];
+let randomColor14 = colors[Math.floor(Math.random() * colors.length)];
+let randomColor15 = colors[Math.floor(Math.random() * colors.length)];
+
+let randomColor16 = colors[Math.floor(Math.random() * colors.length)];
+let randomColor17 = colors[Math.floor(Math.random() * colors.length)];
+let randomColor18 = colors[Math.floor(Math.random() * colors.length)];
+let randomColor19 = colors[Math.floor(Math.random() * colors.length)];
+let randomColor20 = colors[Math.floor(Math.random() * colors.length)];
+
+let angles = ["30deg", "60deg", "90deg", "0deg"] 
+
+let randomRotate1 = angles[Math.floor(Math.random() * angles.length)];
+let randomRotate2 = angles[Math.floor(Math.random() * angles.length)];
+let randomRotate3 = angles[Math.floor(Math.random() * angles.length)];
+let randomRotate4 = angles[Math.floor(Math.random() * angles.length)];
+let randomRotate5 = angles[Math.floor(Math.random() * angles.length)];
+
+let randomRotate6 = angles[Math.floor(Math.random() * angles.length)];
+let randomRotate7 = angles[Math.floor(Math.random() * angles.length)];
+let randomRotate8 = angles[Math.floor(Math.random() * angles.length)];
+let randomRotate9 = angles[Math.floor(Math.random() * angles.length)];
+let randomRotate10 = angles[Math.floor(Math.random() * angles.length)];
+
+let randomRotate11 = angles[Math.floor(Math.random() * angles.length)];
+let randomRotate12 = angles[Math.floor(Math.random() * angles.length)];
+let randomRotate13 = angles[Math.floor(Math.random() * angles.length)];
+let randomRotate14 = angles[Math.floor(Math.random() * angles.length)];
+let randomRotate15 = angles[Math.floor(Math.random() * angles.length)];
+
+let randomRotate16 = angles[Math.floor(Math.random() * angles.length)];
+let randomRotate17 = angles[Math.floor(Math.random() * angles.length)];
+let randomRotate18 = angles[Math.floor(Math.random() * angles.length)];
+let randomRotate19 = angles[Math.floor(Math.random() * angles.length)];
+let randomRotate20 = angles[Math.floor(Math.random() * angles.length)];
+
+tri1.style.transform = randomRotate1;
+tri2.style.transform = randomRotate2;
+tri3.style.transform = randomRotate3;
+tri4.style.transform = randomRotate4;
+tri5.style.transform = randomRotate5;
+
+tri6.style.transform = randomRotate6;
+tri7.style.transform = randomRotate7;
+tri8.style.transform = randomRotate8;
+tri9.style.transform = randomRotate9;
+tri10.style.transform = randomRotate10;
+
+tri11.style.transform = randomRotate11;
+tri12.style.transform = randomRotate12;
+tri13.style.transform = randomRotate13;
+tri14.style.transform = randomRotate14;
+tri15.style.transform = randomRotate15;
+
+tri16.style.transform = randomRotate16;
+tri17.style.transform = randomRotate17;
+tri18.style.transform = randomRotate18;
+tri19.style.transform = randomRotate19;
+tri20.style.transform = randomRotate20;
+
+tri1.style.borderBlockColor = randomColor1;
+tri2.style.borderBlockColor = randomColor2;
+tri3.style.borderBlockColor = randomColor3;
+tri4.style.borderBlockColor = randomColor4;
+tri5.style.borderBlockColor = randomColor5;
+
+tri6.style.borderBlockColor = randomColor6;
+tri7.style.borderBlockColor = randomColor7;
+tri8.style.borderBlockColor = randomColor8;
+tri9.style.borderBlockColor = randomColor9;
+tri10.style.borderBlockColor = randomColor10;
+
+tri11.style.borderBlockColor = randomColor11;
+tri12.style.borderBlockColor = randomColor12;
+tri13.style.borderBlockColor = randomColor13;
+tri14.style.borderBlockColor = randomColor14;
+tri15.style.borderBlockColor = randomColor15;
+
+tri16.style.borderBlockColor = randomColor16;
+tri17.style.borderBlockColor = randomColor17;
+tri18.style.borderBlockColor = randomColor18;
+tri19.style.borderBlockColor = randomColor19;
+tri20.style.borderBlockColor = randomColor20;
+
+
+
+console.log("hello")
