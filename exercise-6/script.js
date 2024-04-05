16ca8852fefc8b8afb21ca41439338bded3a72f9
leejia0704 <145470836+Jiaalee@users.noreply.github.com>
2024-04-05 01:57:50 -0400

    exercise 6

--- exercise-6/script.js
+++ exercise-6/script.js
@@ -1,6 +1,6 @@
 let squares = document.querySelectorAll(".square-1, .square-2, .square-3, .square-4");
 
-let colors = ["#cdb4db", "#ffc8dd", "#ffafcc", "#bde0fe", "#a2d2ff", "#caf0f8", "#90e0ef", "#00b4d8", "#0077b6", "#03045e"];
+let colors = ["#cdb4db", "#ffc8dd", "#ffafcc", "#bde0fe", "#a2d2ff", "#caf0f8", "#90e0ef", "#00b4d8", "#0077b6", "#03045e", "#edede9", "#d6ccc2", "#f5ebe0", "#e3d5ca", "#d5bdaf"];
 
 for (let square of squares) {
     square.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
